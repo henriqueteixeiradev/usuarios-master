6a106f5e6b8b729b4b1253ba47c4a85ce447ff68
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2022-04-25 17:48:53 -0300

    ajustes no envio da imagem padrão e ta add inner html com appendChild

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,7 +44,11 @@ class UserController {
         reject(e);
       };
 
-      fileReader.readAsDataURL(file);
+      if (file) {
+        fileReader.readAsDataURL(file);
+      } else {
+        resolve("dist/img/boxed-bg.jpg");
+      }
     });
   }
 
@@ -56,6 +60,8 @@ class UserController {
         if (field.checked) {
           user[field.name] = field.value;
         }
+      } else if (field.name === "admin") {
+        user[field.name] = field.checked;
       } else {
         user[field.name] = field.value;
       }
@@ -74,12 +80,16 @@ class UserController {
   }
 
   addLine(dataUser) {
-    this.tableEl.innerHTML = `
+    let tr = document.createElement("tr");
+
+    tr.innerHTML = `
             <tr>
-                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${
+                  dataUser.photo
+                }" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
+                <td>${dataUser.admin ? "Sim" : "Não"}</td>
                 <td>${dataUser.birth}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
@@ -87,5 +97,7 @@ class UserController {
                 </td>
             </tr>
         `;
+
+    this.tableEl.appendChild(tr);
   }
 }
