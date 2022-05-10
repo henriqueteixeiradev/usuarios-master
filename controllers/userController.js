30804b0584cd9261c778671c9a8b6ce6e1699e04
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2022-05-10 19:46:50 -0300

    passando de sessionStorage para localStorage

--- controllers/userController.js
+++ controllers/userController.js
@@ -161,8 +161,8 @@ class UserController {
 
   getUsersStorage() {
     let users = [];
-    if (sessionStorage.getItem("users")) {
-      users = JSON.parse(sessionStorage.getItem("users"));
+    if (localStorage.getItem("users")) {
+      users = JSON.parse(localStorage.getItem("users"));
     }
     return users;
   }
@@ -181,7 +181,7 @@ class UserController {
     let users = this.getUsersStorage();
 
     users.push(data);
-    sessionStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("users", JSON.stringify(users));
   }
 
   addLine(dataUser) {
